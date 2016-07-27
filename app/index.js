3d0f8fe0d613d2350c6b468dfc5c5af2886b1a3e
oscar <1208442830@qq.com>
2016-07-27 21:21:41 +0800

    react

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Index from './components/index/Index';
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('example')
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+	entry:[
+		path.resolve(__dirname, 'app/index.js')
+	],
+	output:{
+		path:path.resolve(__dirname,'dist'),
+		filename:'bundle.js',
+		publicPath: "/dist/"
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.jsx?$/,
+				loaders:['babel'],
+				exclude:/(node_modules|bower_components)/
+			},
+			{
+				test:/.less$/,
+				loaders:['style','css','less']
+			}
+		]
+	},
+	resolve:{
+		extensions: ['', '.js', '.jsx']
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+    ]
+}
+
+module.exports = config;
