742f6111e83ce432e9353190ca33d35cbb0776ec
oscar <1208442830@qq.com>
2016-07-29 16:32:00 +0800

    commit

--- app/index.js
+++ app/index.js
@@ -1,16 +1,12 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore } from 'redux';
 
 import Index from './components/index/Index';
 
 ReactDOM.render(
   <Index />,
   document.getElementById('example')
-);*/
+);
 
-var sub = require('./components/sidebar'); 
-var app = document.createElement('div'); 
-app.innerHTML = '<h1>Hello World!</h1>'; 
-app.appendChild(sub()); 
-document.body.appendChild(app);
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,27 @@ var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
-	entry:[/*
-		'webpack/hot/dev-server',
-		'webpack-dev-server/client?http://localhost:8080',*/
-		path.resolve(__dirname, 'app/index.js')
-	],
+	entry:{
+		index:[
+			'webpack/hot/only-dev-server',
+			'webpack-dev-server/client?http://localhost:8080',
+			path.resolve(__dirname, 'app/index.js')
+		],
+		common:['react','react-dom','redux']
+	},
 	output:{
 		path:path.resolve(__dirname,'build'),
-		filename:'bundle.js',
+		filename:'[name].js',
+		chunkFilename:"[name].js"
 	},
 	module:{
 		loaders:[
 			{
 				test:/\.jsx?$/,
-				loaders:['babel'],
+				loaders:['react-hot','babel'],
 				exclude:/(node_modules|example)/
 			},{
-				test:/.less$/,
+				test:/.less$/, 
 				loaders:['style','css','less']
 			}
 		]
@@ -29,19 +33,15 @@ var config = {
 		extensions: ['', '.js', '.jsx']
 	},
 	plugins: [
-		//new webpack.optimize.CommonsChunkPlugin("vendor","vendor.bundle.js"),
-		new webpack.HotModuleReplacementPlugin(),
+		new webpack.HotModuleReplacementPlugin(),//模块热替换
+		new webpack.NoErrorsPlugin(),//错误不打断编译
+		new webpack.optimize.CommonsChunkPlugin("common","common.bundle.js"),//提取模块公共
 		new HtmlWebpackPlugin({
-			title:'hello react'
-		}),
-		//new OpenBrowserPlugin({ url: 'http://localhost:8080/' })
-    ]/*,
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
-	}*/
+			title:'hello react',
+			template:'./app/index.html'
+		}),//生成html文件
+		new OpenBrowserPlugin({ url: 'http://localhost:8080' })//浏览器打开
+    ]
 }
 
 module.exports = config;
