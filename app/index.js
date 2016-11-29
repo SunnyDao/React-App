faf84140a1781c8f1067addc260df6513ac82676
oscar <1208442830@qq.com>
2016-11-29 21:01:02 +0800

    combineReducers

--- app/index.js
+++ app/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import Counter from './components/Counter/';
-import counter from './reducers';
+import reducer from './reducers/index';
 
-const store = createStore(counter);
+const store = createStore(reducer);
 const rootElement = document.getElementById('example');
 
 const render = () => ReactDOM.render(
   <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    value={store.getState().counter}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT'})}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT'})}
   />,
   rootElement
 )
 
 render();
 store.subscribe(render);
+
+store.dispatch({
+  type: 'INCREMENT'
+})
+console.log(store.getState())
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
-export default (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
+import {combineReducers} from 'redux';
+import todos from './todos';
+import counter from './counter';
+
+export default combineReducers({
+	todos,
+	counter
+})
+
+
