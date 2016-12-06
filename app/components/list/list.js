51cef5f65cb5dfa03a117093bf8da0dcfe1b1aa4
oscar <1208442830@qq.com>
2016-12-06 23:55:17 +0800

    commit

--- app/components/list/list.js
+++ app/components/list/list.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react';
 import ListItem from './item';
 
 class List extends Component {
-    
+    function({items}){
+        items = items.map(
+            item=>(
+                <ListItem
+                    item={item}
+                    key={item.id}
+                />
+            )
+        )
+    }
     render() {
         return (
             <div className="list-component">
