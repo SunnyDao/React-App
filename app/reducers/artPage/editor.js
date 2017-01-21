0ac4250dfab9f19331f21f549caa3a2fe587bf57
oscar <1208442830@qq.com>
2017-01-21 23:48:36 +0800

    commit

--- app/reducers/artPage/editor.js
+++ app/reducers/artPage/editor.js
@@ -5,11 +5,76 @@ const initialState = {
     isEditing: false,
 };
 
-export default function editor(state=initialState,action){
-    switch (action.type){
-        case ActionTypes.ART_CREATE_ITEM:
-            return Object.assign({},state,{selectedId:null,editing:true})
-        default:
-            return state;
-    }
+const { pendingOf, fulfilledOf } = ActionTypes;
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  let id;
+  let entry;
+
+  switch (type) {
+
+    case pendingOf(ActionTypes.FETCH_ENTRY):
+      id = payload;
+      return {
+        ...state,
+        [id]: {
+          ...state[id],
+          isFetching: true,
+        },
+      };
+
+    case fulfilledOf(ActionTypes.FETCH_ENTRY):
+      entry = payload;
+      return {
+        ...state,
+        [entry.id]: {
+          data: entry,
+          isFetching: false,
+        },
+      };
+
+    case pendingOf(ActionTypes.SAVE_ENTRY):
+      id = payload.id;
+      return {
+        ...state,
+        [id]: {
+          ...state[id],
+          isFetching: true,
+        },
+      };
+
+    case fulfilledOf(ActionTypes.SAVE_ENTRY):
+      entry = payload;
+      return {
+        ...state,
+        [entry.id]: {
+          data: entry,
+          isFetching: false,
+        },
+      };
+
+    case pendingOf(ActionTypes.DELETE_ENTRY):
+      id = payload;
+      return {
+        ...state,
+        [id]: {
+          ...state[id],
+          isFetching: true,
+        },
+      };
+
+    case fulfilledOf(ActionTypes.DELETE_ENTRY):
+      id = payload;
+      return {
+        ...state,
+        [id]: {
+          data: null,
+          isFetching: false,
+        },
+      };
+
+    default:
+      return state;
+  }
 }
