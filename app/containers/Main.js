48522786710d7d86f94e5202ce08687647a9e730
oscar <1208442830@qq.com>
2017-02-07 22:19:44 +0800

    路由初步完成

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,17 +1,33 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 
+import './style.less';
+
 function Main(props) {
-  return (
-    <div>
-      <ul>
-        <li><IndexLink to="/" activeStyle={{ color: 'red' }}>Home</IndexLink></li>
-        <li><Link to="/counter" activeStyle={{ color: 'red' }}>Counter</Link></li>
-      </ul>
-      {/* this will render the child routes */}
-      {React.cloneElement(props.children, props)}
-    </div>
-  );
+    return (
+      <div>
+        <div className="header">
+          <div className="nav-header">
+            <a href="" className="logo">ReactApp</a>
+          </div>
+          <div className="nav-body">
+            <nav>
+              <IndexLink to="/" activeClassName="active">首页</IndexLink>
+              <Link to="/artpage" activeClassName="active">文章</Link>
+              <Link to="/counter" activeClassName="active">图书</Link>
+              <Link to="/about" activeClassName="active">关于我</Link>
+            </nav>
+          </div>
+          <div className="nav-footer">
+            <div className="login">登录</div>
+          </div>
+        </div>
+        <div className="containers">
+          {/* this will render the child routes */}
+          {React.cloneElement(props.children, props)}
+        </div>
+        </div>
+    )
 }
 Main.propTypes = {
   children: PropTypes.any.isRequired
--- app/router/index.js
+++ app/router/index.js
@@ -3,11 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 
 import Main from '../containers/Main';
 import Home from '../containers/Home';
+import ArtPage from '../containers/ArtPage';
 import Counter from '../containers/Counter';
 
 export default () => (
     <Route path="/" component={Main}>
         <IndexRoute component={Home} />
+        <Route path="artpage" component={ArtPage} />
         <Route path="counter" component={Counter} />
     </Route>
 );
