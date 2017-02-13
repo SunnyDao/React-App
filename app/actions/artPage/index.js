c8e911a6388356dfd48d14c5eaaaaab076c5030c
oscar <1208442830@qq.com>
2017-02-13 23:55:54 +0800

    11111

--- app/actions/artPage/index.js
+++ app/actions/artPage/index.js
@@ -60,3 +60,35 @@ export function fetchEntryList(url) {
 function fetchItems(url){
     return axios.get(url);
 }
+
+export function fetchEntryList() {
+  return dispatch => {
+    storage.getAll()
+      .then(items => dispatch(updateEntryList(items)));
+  };
+}
+
+export const UPDATE_SAVED_ENTRY = 'UPDATE_SAVED_ENTRY';
+
+function updateSavedEntry(id) {
+  return { type: UPDATE_SAVED_ENTRY, id };
+}
+
+export function saveEntry(item) {
+  const { title, content, id } = item;
+  return dispatch => {
+    if (id) {
+      // 更新流程
+      storage.updateEntry(id, title, content)
+        .then(() => dispatch(updateSavedEntry(id)))
+        .then(() => storage.getAll())
+        .then(items => dispatch(updateEntryList(items)));
+    } else {
+      // 创建流程
+      storage.insertEntry(title, content)
+        .then(inserted => dispatch(updateSavedEntry(inserted.id)))
+        .then(() => storage.getAll())
+        .then(items => dispatch(updateEntryList(items)));
+    }
+  };
+}
